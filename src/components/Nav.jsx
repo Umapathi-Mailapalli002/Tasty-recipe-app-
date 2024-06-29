760438ffcaeff4780756042c536975af5e5a28e6
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2024-06-29 12:05:51 +0530

    changed nav from sticky to fixed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -12,7 +12,7 @@ function Nav() {
   return (
     <>
           <div className="absolute w-full h-full bg-black opacity-60"></div>
-    <div className='sticky top-0'>
+    <div className='fixed w-full top-0'>
         
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto md:p-4 sm:p-4 lg:p-4 p-2">
@@ -94,7 +94,7 @@ function Nav() {
              
               className="block py-2 px-3  active:bg-blue-700 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent  active:md:text-blue-700 md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
             >
-              Explore 
+              Explore
             </a>
           </li>
           <li>
